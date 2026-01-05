778f27483a0ce18c943944482e60788f1b08cbca
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2026-01-04 21:13:14 -0700

    444

---
+++ components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="flex justify-between">
+        <div>collaspable button</div>
+        <div>
+          <div className="flex">
+            <div>Dashboard</div>
+            <div>Dashboard</div>
+            <div>Dashboard</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
