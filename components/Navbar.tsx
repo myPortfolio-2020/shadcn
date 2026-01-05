9fac11e3c2cebc34bcb705926c14492ca86fa692
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2026-01-04 21:26:58 -0700

    hhh

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,13 @@
+import { BellRing, MoonStar } from "lucide-react";
 import React from "react";
-
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 const Navbar = () => {
   return (
     <>
@@ -7,9 +15,25 @@ const Navbar = () => {
         <div>collaspable button</div>
         <div>
           <div className="flex">
-            <div>Dashboard</div>
-            <div>Dashboard</div>
-            <div>Dashboard</div>
+            <div className="m-2">
+              <BellRing />
+            </div>
+            <div className="m-2">
+              <MoonStar />
+            </div>
+            <div className="m-2">
+              <DropdownMenu>
+                <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Profile</DropdownMenuItem>
+                  <DropdownMenuItem>Billing</DropdownMenuItem>
+                  <DropdownMenuItem>Team</DropdownMenuItem>
+                  <DropdownMenuItem>Subscription</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
           </div>
         </div>
       </div>
